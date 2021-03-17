aa14d5e969e47ff52b18650978b6339e04e958c4
Leead Arbetman <liad180895@gmail.com>
2021-03-17 12:04:25 +0200

    Creating About page
    - Creating about.html file
    - Adding about method in views.py
    - Adding a path to the page in urls.py

--- apphomepage/views.py
+++ apphomepage/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'apphomepage/homepage.html')
+
+
+def about(request):
+    return render(request, 'apphomepage/about.html')
--- picATrip/urls.py
+++ picATrip/urls.py
@@ -19,5 +19,6 @@ from apphomepage import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('about/', views.about, name='homepage')
 ]
