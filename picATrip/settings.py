22a234a2a08fa7526f9d69ce09ce883395bf100d
Leead Arbetman <liad180895@gmail.com>
2021-03-30 16:20:54 +0300

    Updating settings.py and urls.py
    - Adding a path in urls.py to the registration page
    - Adding the users app to the INSTALLED_APPS in setting.py

--- picATrip/settings.py
+++ picATrip/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pickATrip_django_apps.apps.pickATrip_django_appsConfig'
+    'pickATrip_django_apps.apps.pickATrip_django_appsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
