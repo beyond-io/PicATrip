c2d1ea9586b0bc38f8ebaf1933b72571b20a6bee
Maayan Hadar <maayan.hadarwork@gmail.com>
2021-04-04 13:49:24 +0300

    Adding a comment model and admin model registration
    - adding [migrations/0001_initial.py][1] and a model ([models.py][2]) for representing a comment.
    - adding customized model registration to comment model and automating the process for models ([admin.py][3]) that will be added in the future
    
    Adding commenting model part and model registration in the admin.py file
    
    related issue: #44
    closes #46

--- pickATrip_django_apps/admin.py
+++ pickATrip_django_apps/admin.py
@@ -1,3 +1,31 @@
-# from django.contrib import admin
+from django.apps import apps
+from django.contrib import admin
 
-# Register your models here.
+
+# Customized model registration
+@admin.register
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('username', 'body', 'created_on', 'tag', 'post', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('username', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+
+# for viewing models registration fields
+class ListAdminMixin(object):
+    def __init__(self, general_model, admin_site):
+        self.list_display = [field.name for field in general_model._meta.get_fields()]
+        super(ListAdminMixin, self).__init__(model, admin_site)
+
+
+# Register all other models automatically - should stay last in file.
+models = apps.get_models()
+for model in models:
+    admin_class = type('AdminClass', (ListAdminMixin, admin.ModelAdmin), {})
+    try:
+        admin.site.register(model, admin_class)
+    except admin.sites.AlreadyRegistered:
+        pass
--- pickATrip_django_apps/models.py
+++ pickATrip_django_apps/models.py
@@ -1,3 +1,31 @@
-# from django.db import models
+from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+# Need to be implemented by Amir/Shoval
+class Post(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    # profile_image = models.ImageField(upload_to='pickATrip_django_apps/profileImages', blank=True)
+
+    class Tag(models.TextChoices):
+        RECOMMENDED = "Recommended"
+        WANT_TO_GO = "Want to go"
+        QUITE = "Quite"
+        CHANCE_TO_MEET = "Chance to meet"
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    username = models.CharField(max_length=50)
+    body = models.TextField()
+    created_on = models.DateTimeField(default=timezone.now)
+    # temporary- might be replaced in a field for supporting a button functionality in later versions
+    tag = models.CharField(choices=Tag.choices, max_length=20, blank=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.username)
