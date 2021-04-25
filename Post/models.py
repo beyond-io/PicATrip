67b17c7b7ac509df720329dd7c4736da35445ef1
Shoval Ovadia <shovalovadia94@gmail.com>
2021-04-25 08:54:51 +0300

    Adding a basic test for model 'Post'
    - added a __str__ function to model.
    
    - created a test for this function.

--- Post/models.py
+++ Post/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     nameOfLocation = models.CharField(max_length=100)
     photoURL = models.TextField()
     Description = models.TextField()
+
+    def __str__(self):
+        return f'{self.nameOfPoster} traveled {self.nameOfLocation} and wrote: {self.Description}'
