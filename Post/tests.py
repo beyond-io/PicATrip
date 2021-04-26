d11fc9b7a71ba8ae23be67728bca4997d572f6b4
Shoval Ovadia <shovalovadia94@gmail.com>
2021-04-26 17:34:37 +0300

    Adding tests for 'Post' model
    - Test for creation
    - Test for deletion
    - Test for 'all_posts' function
    - Test for the comments of a post
    
    I added these tests so we can test the functionality of 'Post' model.

--- Post/tests.py
+++ Post/tests.py
@@ -1,19 +1,14 @@
 from Post.models import Post
 from .forms import CreatePostForm
+from django.db.models.query import QuerySet
+from commenting_system.models import Comment
 import pytest
 
 
-class TestPost:
-    def test_post_creation(self):
+@pytest.mark.django_db
+def test_post_str(post):
 
-        post1 = Post(
-            nameOfPoster='Shoval',
-            nameOfLocation='The Dead Sea',
-            photoURL='www.testPost.com',
-            Description='beautiful place',
-        )
-
-        assert str(post1) == "Shoval traveled The Dead Sea and wrote: beautiful place"
+    assert str(post) == "Shoval traveled The Dead Sea and wrote: beautiful place"
 
 
 @pytest.mark.django_db
@@ -27,6 +22,57 @@ def test_post_form(form):
     assert post.nameOfLocation == 'Israel'
     assert post.photoURL == 'www.test.com'
     assert post.Description == 'cool place'
+
+
+@pytest.mark.django_db
+def test_post_creation(post):
+    post.save()
+
+    assert Post.objects.filter(pk=post.id).exists()  # checks if the post is saved
+    assert Post.objects.get(pk=post.id) == post  # check if the post saved correctly
+
+
+@pytest.mark.django_db
+def test_post_delete(post):
+    post.save()
+
+    assert Post.objects.filter(pk=post.id).exists()
+    Post.objects.get(pk=post.id).delete()
+    assert Post.objects.filter(pk=post.id).exists() is False
+
+
+@pytest.mark.django_db
+def test_all_posts():
+
+    posts = Post.all_posts()
+
+    assert isinstance(posts, QuerySet)
+    assert all(isinstance(post, Post) for post in posts)
+    assert all(post is not None for post in posts)
+    assert all(len(post.Description) > 0 for post in posts)
+    assert all(len(post.nameOfLocation) > 0 for post in posts)
+    assert all(len(post.nameOfPoster) > 0 for post in posts)
+    assert all(len(post.photoURL) > 0 for post in posts)
+
+
+@pytest.mark.django_db
+def test_post_comments():
+    posts = Post.all_posts()
+
+    for post in posts:
+        assert isinstance(post.comments.all(), QuerySet)
+        assert all(isinstance(comment, Comment) for comment in post.comments.all())
+
+
+@pytest.fixture
+def post():
+
+    return Post(
+        nameOfPoster='Shoval',
+        nameOfLocation='The Dead Sea',
+        photoURL='www.testPost.com',
+        Description='beautiful place',
+    )
 
 
 @pytest.fixture
