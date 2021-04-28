da3ea8ebba6e811800c444f75c2af8ae4b3435b9
Shoval Ovadia <shovalovadia94@gmail.com>
2021-04-28 09:26:05 +0300

    Update postList.html to contain images
    * A post on 'localhost:8000/postList' contains a photo -  'Post/templates/Post/postList.html' modified.
    * Update 'test data' so the URL will be a valid one - 'Post/migrations/0002_test_data.py' modified.

--- Post/migrations/0002_test_data.py
+++ Post/migrations/0002_test_data.py
@@ -11,12 +11,14 @@ class Migration(migrations.Migration):
 
         p1_Author = 'Shoval'
         p1_Place = 'Eilat'
-        p1_URL = 'www.test.com'
+        p1_URL = (
+            "https://israel.travel/wp-content/uploads/2019/02/eilatnewinside-min.jpg"
+        )
         p1_Description = 'This is my favorite place! chill vibes and beautiful sea.'
 
         p2_Author = 'Daniel'
         p2_Place = 'Dead Sea'
-        p2_URL = 'www.test2.com'
+        p2_URL = "https://velvetescape.com/wp-content/uploads/2011/11/IMG_2370-3-1280x920.jpg"
         p2_Description = 'Beautiful place.'
 
         with transaction.atomic():
