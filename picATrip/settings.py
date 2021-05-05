eb144e7e9f315d8ea2fddf86de12276be0bae88a
Leead Arbetman <liad180895@gmail.com>
2021-05-05 12:21:32 +0300

    Delete unused media files and Resize profile pictures
    - Adding django-cleanup app
    - Creating save method in models.py

--- picATrip/settings.py
+++ picATrip/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'pickATrip_django_apps.apps.pickATrip_django_appsConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'django_cleanup',
     'Post.apps.PostConfig',
     'commenting_system.apps.CommentingSystemConfig',
 ]
