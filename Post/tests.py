d6df7e1c496d89e45f74c4c18121758526709b3d
Shoval Ovadia <shovalovadia94@gmail.com>
2021-05-15 12:28:21 +0300

    Adding tests for 'post detail' feature
    -One test for a logged-in user and another one for a user that didn't log in.
    
    Post/tests.py modified.

--- Post/tests.py
+++ Post/tests.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 import pytest
 from django.urls import reverse
 from django.test import TestCase
-from Post.views import CreateNewPost
+from Post.views import CreateNewPost, post_detail
 
 
 @pytest.mark.django_db
@@ -197,3 +197,49 @@ class CreatePostTest(TestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/login/?next=/createPost/')
+
+
+@pytest.mark.django_db
+class ViewPostTest(TestCase):
+    def test_post_detail(self):
+
+        new_user = User.objects.create_user(
+            username='Roni', email='Test26@gmail.com', password='password2266'
+        )
+        new_user.save()
+
+        self.client.login(username='Roni', password='password2266')
+
+        post = Post(
+            user=new_user,
+            nameOfLocation='Israel',
+            photoURL='www.test.com',
+            Description='cool place',
+        )
+        post.save()
+
+        response = self.client.get(reverse('post_detail', kwargs={'post_id': post.id}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.resolver_match.func, post_detail)
+
+    def test_failed_post_detail(self):
+        Post.objects.all().delete()
+
+        new_user = User.objects.create_user(
+            username='Roni', email='Test26@gmail.com', password='password2266'
+        )
+        new_user.save()
+
+        post = Post(
+            user=new_user,
+            nameOfLocation='Israel',
+            photoURL='www.test.com',
+            Description='cool place',
+        )
+        post.save()
+
+        response = self.client.get(reverse('post_detail', kwargs={'post_id': post.id}))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'/login/?next=/postList/{post.id}/')
