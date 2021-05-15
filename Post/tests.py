f50fe00a6a04c7ac5646bf88b60c95d0e026d751
Shoval Ovadia <shovalovadia94@gmail.com>
2021-05-15 12:21:56 +0300

    Adding tests for 'Create a post' feature
    - Post/tests.py modified.

--- Post/tests.py
+++ Post/tests.py
@@ -5,6 +5,8 @@ from commenting_system.models import Comment
 from django.contrib.auth.models import User
 import pytest
 from django.urls import reverse
+from django.test import TestCase
+from Post.views import CreateNewPost
 
 
 @pytest.mark.django_db
@@ -149,3 +151,49 @@ def test_delete_post(client):
     )
 
     assert response.status_code == 302
+
+
+@pytest.mark.django_db
+class CreatePostTest(TestCase):
+    def test_create_post(self):
+        Post.objects.all().delete()
+
+        new_user = User.objects.create_user(
+            username='Roni', email='Test26@gmail.com', password='password2266'
+        )
+        new_user.save()
+
+        self.client.login(username='Roni', password='password2266')
+
+        response = self.client.post(
+            reverse('createPost'),
+            {
+                'nameOfLocation': 'Dead Sea',
+                'photoURL': 'www.test123.com',
+                'Description': 'Amazing!',
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/postList/')
+
+        post = Post.objects.all()
+        self.assertEqual(post[0].user, new_user)
+        self.assertEqual(post[0].nameOfLocation, 'Dead Sea')
+        self.assertEqual(post[0].photoURL, 'www.test123.com')
+        self.assertEqual(post[0].Description, 'Amazing!')
+
+        self.assertEqual(response.resolver_match.func, CreateNewPost)
+
+    def test_failed_create_post(self):
+
+        new_user = User.objects.create_user(
+            username='Roni', email='Test26@gmail.com', password='password2266'
+        )
+        new_user.save()
+        self.client.logout()
+
+        response = self.client.get(reverse('createPost'))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/login/?next=/createPost/')
